92e907759cc41e82717b1aff4f3d80c701d3c23d
Bebop <nneven@usc.edu>
2023-10-21 17:28:36 -0400

    profile page css

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,5 +1,7 @@
 import Head from "next/head";
+import Image from "next/image";
 import { api } from "~/utils/api";
+import { PageLayout } from "~/components/layout";
 
 export default function ProfilePage({ username }: { username: string }) {
   const { data } = api.profile.getUserByUsernames.useQuery({
@@ -13,9 +15,20 @@ export default function ProfilePage({ username }: { username: string }) {
       <Head>
         <title>{data.username}</title>
       </Head>
-      <main className="flex h-screen justify-center">
-        <div>{data.username}</div>
-      </main>
+      <PageLayout>
+        <div className="relative h-36 bg-slate-600">
+          <Image
+            src={data.imageUrl}
+            alt={`${data.username}'s profile image}`}
+            className="absolute bottom-0 left-0 -mb-16 ml-4 rounded-full border-4 border-black"
+            width={128}
+            height={128}
+          />
+        </div>
+        <div className="h-16"></div>
+        <div className="p-4 text-2xl font-bold">{`@${data.username}`}</div>
+        <div className="w-full border-b border-slate-400"></div>
+      </PageLayout>
     </>
   );
 }
