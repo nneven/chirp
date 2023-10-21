160fe8d11f9c4d48ce549a53eb287cc1c76ab914
Bebop <nneven@usc.edu>
2023-10-21 16:51:02 -0400

    createServerSideHelpers

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -20,3 +20,35 @@ export default function ProfilePage() {
     </>
   );
 }
+
+import type { GetStaticProps } from "next";
+import { createServerSideHelpers } from "@trpc/react-query/server";
+import { appRouter } from "~/server/api/root";
+import { db } from "~/server/db";
+import superjson from "superjson";
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const ssg = createServerSideHelpers({
+    router: appRouter,
+    ctx: { db, userId: null },
+    transformer: superjson,
+  });
+
+  const slug = context.params?.slug;
+
+  if (typeof slug !== "string") throw new Error("no slug");
+
+  const username = slug.replace("@", "");
+
+  await ssg.profile.getUserByUsernames.prefetch({ username });
+
+  return {
+    props: {
+      trpcState: ssg.dehydrate(),
+    },
+  };
+};
+
+export const getStaticPaths = () => {
+  return { paths: [], fallback: "blocking" };
+};
