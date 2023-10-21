ed686a051c0fab8ffabb2e6361e1f2197e963b46
Bebop <nneven@usc.edu>
2023-10-21 18:00:59 -0400

    Update [id].tsx

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -1,14 +1,87 @@
 import Head from "next/head";
+import Image from "next/image";
+import { api } from "~/utils/api";
+import { PageLayout } from "~/components/layout";
+import { PostView } from "~/components/postview";
 
-export default function SinglePostPage() {
+const ProfileFeed = (props: { userId: string }) => {
+  const { data, isLoading } = api.post.getPostsByUserId.useQuery({
+    userId: props.userId,
+  });
+
+  if (isLoading) return <LoadingPage />;
+  if (!data || data.length === 0) return <div>User has not posted</div>;
+
+  return (
+    <div className="flex flex-col gap-4">
+      {data.map((fullPost) => (
+        <PostView key={fullPost.post.id} {...fullPost} />
+      ))}
+    </div>
+  );
+};
+
+export default function ProfilePage({ username }: { username: string }) {
+  const { data } = api.profile.getUserByUsernames.useQuery({
+    username,
+  });
+
+  if (!data) return <div>404</div>;
+
   return (
     <>
       <Head>
-        <title>Post</title>
+        <title>{data.username}</title>
       </Head>
-      <main className="flex h-screen justify-center">
-        <div>Post View</div>
-      </main>
+      <PageLayout>
+        <div className="relative h-36 bg-slate-600">
+          <Image
+            src={data.imageUrl}
+            alt={`${data.username}'s profile image}`}
+            className="absolute bottom-0 left-0 -mb-16 ml-4 rounded-full border-4 border-black bg-black"
+            width={128}
+            height={128}
+          />
+        </div>
+        <div className="h-16"></div>
+        <div className="p-4 text-2xl font-bold">{`@${data.username}`}</div>
+        <div className="w-full border-b border-slate-400"></div>
+        <ProfileFeed userId={data.id} />
+      </PageLayout>
     </>
   );
 }
+
+import type { GetStaticProps } from "next";
+import { createServerSideHelpers } from "@trpc/react-query/server";
+import { appRouter } from "~/server/api/root";
+import { db } from "~/server/db";
+import superjson from "superjson";
+import LoadingPage from "~/components/loading";
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const ssg = createServerSideHelpers({
+    router: appRouter,
+    ctx: { db, userId: null },
+    transformer: superjson,
+  });
+
+  const slug = context.params?.slug;
+
+  if (typeof slug !== "string") throw new Error("no slug");
+
+  const username = slug.replace("@", "");
+
+  await ssg.profile.getUserByUsernames.prefetch({ username });
+
+  return {
+    props: {
+      trpcState: ssg.dehydrate(),
+      username,
+    },
+  };
+};
+
+export const getStaticPaths = () => {
+  return { paths: [], fallback: "blocking" };
+};
